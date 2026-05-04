1abd79f5655a3543827f5e4697823319a0ddd0b5
Manh Nguyen <manhnb1911@gmail.com>
2026-05-04 17:33:09 +0700

    1.2.1

--- Scripts/IAudioManagerSettings.cs
+++ Scripts/IAudioManagerSettings.cs
@@ -1,32 +1,12 @@
 ﻿#nullable enable
 namespace UniT.Audio
 {
-    using System;
-
     public interface IAudioManagerSettings
     {
-        public event Action SoundVolumeChanged;
+        public AudioSettings MasterSettings { get; }
 
-        public event Action MuteSoundChanged;
+        public AudioSettings SoundSettings { get; }
 
-        public event Action MusicVolumeChanged;
-
-        public event Action MuteMusicChanged;
-
-        public event Action MasterVolumeChanged;
-
-        public event Action MuteMasterChanged;
-
-        public float SoundVolume { get; set; }
-
-        public bool MuteSound { get; set; }
-
-        public float MusicVolume { get; set; }
-
-        public bool MuteMusic { get; set; }
-
-        public float MasterVolume { get; set; }
-
-        public bool MuteMaster { get; set; }
+        public AudioSettings MusicSettings { get; }
     }
 }
